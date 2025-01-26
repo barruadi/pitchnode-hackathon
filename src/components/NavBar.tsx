dba8f99a1388f6cd0e5fff902727bdc293100fd0
Barru Adi <barru.adi@gmail.com>
2025-01-26 18:10:25 +0700

    feat: navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,23 @@
-import "./index.css";
+import "../index.css";
+import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar() {    
     return (
-        <div>
-            navbar
+        <div className="flex justify-between items-center w-full p-2 border-2 border-black rounded-2xl shadow-md">
+            <div className="font-bold px-5">
+                <Link to="/">PitchNode</Link>
+            </div>
+            <div className="flex space-x-40 items-center">
+                <Link to="/invest">Invest</Link>
+                <Link to="/learn">Learn</Link>
+                <Link to="/about">About</Link>
+                <div className="flex space-x-10 items-center">
+                    <Link to="/signin" className="font-bold">Login</Link>
+                    <Link to="/signup" className="rounded-md bg-[#324286] text-white px-5 py-1">Register</Link>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Navbar;
