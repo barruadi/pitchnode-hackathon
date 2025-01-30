c4521cfa7fe59466576c2f912777bafaaebcf176
azfaradhi <aradhihakim@gmail.com>
2025-01-30 14:49:18 +0700

    adding Business Idea Card

--- src/components/IdeaCard.tsx
+++ src/components/IdeaCard.tsx
@@ -2,8 +2,50 @@ import "./index.css";
 
 function IdeaCard() {
     return (
-        <div>
-            ideacard
+        <div className="border-2 border-gray-200 rounded-2xl shadow-lg w-[350px] mx-auto flex flex-col bg-white">
+            <div className="">
+                <img src="./image.png" alt="Idea Image" className="w-full" />
+            </div>
+            <div className="p-4">
+                <h2 className="text-xl font-bold text-gray-900">Idea Title #1</h2>
+                <p className="text-black text-sm mt-2">
+                    General overview of the idea. They can showcase their unique and innovative ideas here to raise funds based on their target etc etc.
+                </p>
+            </div>
+
+            <div className="px-4">
+                <div className="flex justify-end items-center text-sm font-bold text-gray-800">
+                    <span className="flex items-center">
+                        <span className="mx-1">69%</span> funded
+                    </span>
+                </div>
+                <div className="relative w-full h-2 mt-2 bg-gray-300 rounded-full">
+                    <div className="absolute h-2 bg-gradient-to-r from-purple-500 to-blue-500 rounded-full" style={{ width: "69%" }}></div>
+                </div>
+            </div>
+
+            <div className="flex justify-between px-4 mt-4 text-center text-sm text-black">
+                <div>
+                    <p className="font-bold text-[#324286]">Indicator 1</p>
+                    <p className="text-lg font-semibold">$123K</p>
+                </div>
+                <div>
+                <p className="font-bold text-[#324286]">Indicator 2</p>
+                <p className="text-lg font-semibold">3-12%</p>
+                </div>
+                <div>
+                <p className="font-bold text-[#324286]">Investors</p>
+                <p className="text-lg font-semibold">12</p>
+                </div>
+            </div>
+            <div className="flex justify-between px-4 py-4">
+                <button className="w-1/2 py-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md mr-2">
+                    Invest
+                </button>
+                <button className="w-1/2 py-2 bg-gray-200 text-gray-800 font-semibold rounded-lg shadow-md">
+                    View More
+                </button>
+            </div>
         </div>
     )
 }
